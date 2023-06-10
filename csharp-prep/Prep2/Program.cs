ed7becafbbd36fc8a40145dc1d62861370af9fb0
thaehae <tmhaehae@gmail.com>
2023-06-11 00:25:38 +1200

    added if & else statements to prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,44 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80 && grade <= 89)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70 && grade <= 79)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 && grade <= 69)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+
+        if (grade >= 70)
+        {
+            Console.Write("Congratulations, you passed this course!");
+        }
+        else 
+        {
+            Console.Write("Please study more for a better grade next time.");
+        }
+
+
     }
 }
