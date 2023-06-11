0d97fb829fcba505e0f49409c35463ef047e034b
thaehae <tmhaehae@gmail.com>
2023-06-11 14:46:27 +1200

    added if & else statements and a do-while loop

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number? ");
+        //int number = int.Parse(Console.ReadLine());
+
+        //Console.Write("What is your guess? ");
+        //int userNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int userNumber = -1;
+
+        do
+        {
+           Console.Write("What is your guess? ");
+           userNumber = int.Parse(Console.ReadLine()); 
+
+            if (number > userNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < userNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }   while (userNumber == number);
     }
 }
