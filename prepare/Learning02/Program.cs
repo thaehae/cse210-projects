540a0043954de896a5ea35ef86ddbb9eabf6fb59
thaehae <tmhaehae@gmail.com>
2023-06-16 13:35:17 +1200

    added jobs to the main and Display

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        //job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._company = "Adaptive";
+        job2._jobTitle = "Software Developer";
+        job2._startYear = 2017;
+        job2._endYear = 2019;
+        //job2.DisplayJobDetails();
+    
+        Resume resume1 = new Resume();
+        resume1._name = "Tracy Haehae";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
+        
     }
+    
+
 }
