c0ce5f53e1c5ccf667f183ccea045b4623a38737
thaehae <tmhaehae@gmail.com>
2023-06-25 17:26:30 +1200

    Main program add text & scripture

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        //Console.WriteLine("Hello Develop03 World!");
+        string userInput = "";
+
+        Reference reference = new Reference("2 Timothy", 1, 7);
+        Scripture scripture = new Scripture(reference, "For God hath not given us the spirit of fear; but of power, and of love, and of a sound mind.");
+
+        while (userInput != "quit")
+        {
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue or type 'quit' to finish");
+            userInput = Console.ReadLine();
+            //scripture.HideRandomWords();
+            //Console.Clear();
+        }
+        
     }
 }
