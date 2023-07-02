7e0d7e75ca4ae9ce535b5db7a8c546693c707734
thaehae <tmhaehae@gmail.com>
2023-07-02 17:21:29 +1200

    Main progran Mindfulness

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        int userMenuInput = 0;
+
+        List<string> menu = new List<string>
+        {
+            "Menu Options:", 
+            " 1. Start breathing activity",
+            " 2. Start reflecting acttivity",
+            " 3. Start listening activity",
+            " 4. Quit"
+        };
+
+        while (userMenuInput != 4)
+        {
+            Console.Clear();
+            foreach (string menuItem in menu)
+            {
+                Console.WriteLine(menuItem);
+            }
+            Console.Write("Select a choice from the menu: ");
+            userMenuInput = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            switch (userMenuInput)
+            {
+                case 1:
+                    BreathingActivity breathingActivity = new BreathingActivity();
+                    breathingActivity.Run();
+                    break;
+                //case 2:
+                    //ReflectingActivity reflectingActivity = new ReflectingActivity();
+                    //reflectingActivity.Run();
+                    //break;
+                //case 3:
+                    //ListeningActivity listentingActivity = new ListeningActivity();
+                    //listeningActivity.Run();
+                    //break;
+            }
+        }
     }
 }
