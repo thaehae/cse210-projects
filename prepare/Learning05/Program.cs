126d061197643f534c4ec3838d772a2fe50deace
thaehae <tmhaehae@gmail.com>
2023-07-06 06:31:47 +1200

    Add program for shapes & colors

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        //Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Gold", 4);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Black", 5, 2.5);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("White", 9);
+        shapes.Add(s3);
+
+        foreach(Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
