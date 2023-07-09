8f100596e6b4657cb6e2e716c9a65d07acc170e4
thaehae <tmhaehae@gmail.com>
2023-07-09 16:58:53 +1200

    Add derived EternalGoal

---
+++ prove/Develop05/EternalGoal.cs
@@ -0,0 +1,27 @@
+using System;
+public class EternalGoal : Goal
+
+{
+
+    public EternalGoal(string stringName, string description, string points) 
+        : base(stringName, description, points)
+    {
+        
+    }
+
+    public void RecordEvent()
+    {
+
+    }
+
+    public bool isComplete()
+    {
+
+    }
+
+    public override string GetStringRepresentation()
+    {
+                
+    }
+
+}
