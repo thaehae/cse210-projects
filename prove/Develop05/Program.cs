77e62f8482fd7366e451caa692fd8e7fb6bf032e
thaehae <tmhaehae@gmail.com>
2023-07-09 16:57:46 +1200

    Add Goals main program

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        int             menuUserInput = 0;
+
+        List<string>    menu = new List<string>
+        {
+            "Menu Options: ",
+            "1. Create new goal",
+            "2. List goals",
+            "3. Save goals",
+            "4. Load goals",
+            "5. Record event",
+            "6. Quit"
+        };
+
+        while(menuUserInput != 6)
+        {
+            foreach (string menuItem in menu)
+            {
+                Console.WriteLine(menuItem);
+            }
+            Console.WriteLine("Select a choice from the menu: ");
+            menuUserInput = int.Parse(Console.ReadLine());
+
+            switch(menuUserInput)
+            {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+            }
+        }
     }
 }
