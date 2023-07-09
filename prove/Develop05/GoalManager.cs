23dbec88cefc2ecd46a8b67cb0d6ada1a111f2b3
thaehae <tmhaehae@gmail.com>
2023-07-09 16:59:26 +1200

    Added GoalManager

---
+++ prove/Develop05/GoalManager.cs
@@ -0,0 +1,55 @@
+using System;
+
+public class GoalManager
+{
+
+    private List<Goal> _goals;
+
+    private int _score;
+
+    public GoalManager(List goals, int score)
+    {
+        _score = score;
+        _goals = goals;
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void DisplayPlayerInfo()
+    {
+
+    }
+
+    public void ListGoalNames()
+    {
+
+    }
+
+    public void ListGoalDetails()
+    {
+
+    }
+
+    public void CreateGoal()
+    {
+
+    }
+
+    public void RecordEvent()
+    {
+
+    }
+
+    public void SaveGoals()
+    {
+
+    }
+
+    public void LoadGoals()
+    {
+
+    }
+}
