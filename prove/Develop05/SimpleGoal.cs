1987c59a6b9863960198974abf8a880538ab5fe6
thaehae <tmhaehae@gmail.com>
2023-07-09 16:58:37 +1200

    Add derived SimpleGoal

---
+++ prove/Develop05/SimpleGoal.cs
@@ -0,0 +1,26 @@
+public class SimpleGoal : Goal
+
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string stringName, string description, string points) 
+        : base(stringName, description, points)
+    {
+
+    }
+
+    public void RecordEvent()
+    {
+
+    }
+
+    public bool IsComplete()
+    {
+
+    }
+
+    public override string GetStringRepresentation()
+    {
+
+    }
+}
