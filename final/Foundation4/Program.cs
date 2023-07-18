97ec2771f10706c488f5c60084268c17eb37e255
thaehae <tmhaehae@gmail.com>
2023-07-18 20:01:44 +1200

    Add Program

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        //Console.WriteLine("Hello Foundation4 World!");
+
+        
     }
 }
